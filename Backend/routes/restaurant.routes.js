db7013f2cc86c7ba7ff03441765bdedb53b5e7ff
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-08-21 20:45:48 +0530

    Add restaurant details API endpoint

--- Backend/routes/restaurant.routes.js
+++ Backend/routes/restaurant.routes.js
@@ -8,16 +8,43 @@ router.get("/", async (req, res) => {
     const restaurants = await Restaurants.find();
 
     res.status(200).json({
-        success: true,
-        count: restaurants.length,
-        restaurants,
+      success: true,
+      count: restaurants.length,
+      restaurants,
     });
   } catch (err) {
-    console.error("Failed to fetch restuarants",err);
+    console.error("Failed to fetch restuarants", err);
 
     res.status(500).json({
+      success: false,
+      msg: "Failed to fetch restuarants",
+    });
+  }
+});
+
+router.get("/:resId", async (req, res) => {
+  try {
+    const { resId } = req.params;
+
+    const restaurant = await Restaurants.findOne({ restaurantId:resId });
+
+    if (!restaurant) {
+      return res.status(404).json({
         success: false,
-        msg: "Failed to fetch restuarants",
+        msg: "Restaurant not  found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      restaurant,
+    });
+  } catch (err) {
+    console.error("Failed to fetch restaurant", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch restaurant",
     });
   }
 });
