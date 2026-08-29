a49a11c72413de7a2e172da1b2c4ff036e17e55a
Mohammed Muqthadir Ahmed <mohammedmuqtadir41@gmail.com>
2026-08-29 15:50:42 +0530

    refactor backend controllers and remove unused code

---
+++ Backend/controllers/restaurant.controller.js
@@ -0,0 +1,49 @@
+const Restaurants = require("../models/restaurant.model");
+
+const getRestaurants = async(req,res) => {
+    try {
+    const restaurants = await Restaurants.find();
+
+    res.status(200).json({
+      success: true,
+      count: restaurants.length,
+      restaurants,
+    });
+  } catch (err) {
+    console.error("Failed to fetch restuarants", err);
+
+    res.status(500).json({
+      success: false,
+      msg: "Failed to fetch restuarants",
+    });
+  }
+}
+
+const getRestaurantById = async (req, res) => {
+    try {
+    const { resId } = req.params;
+
+    const restaurant = await Restaurants.findOne({ restaurantId:resId });
+
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        msg: "Restaurant not  found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      restaurant,
+    });
+  } catch (err) {
+    console.error("Failed to fetch restaurant", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch restaurant",
+    });
+  }
+}
+
+module.exports = {getRestaurants ,getRestaurantById}
--- Backend/routes/restaurant.routes.js
+++ Backend/routes/restaurant.routes.js
@@ -1,52 +1,12 @@
 const express = require("express");
+const {getRestaurantById, getRestaurants} = require("../controllers/restaurant.controller")
+
+
 const router = express.Router();
 
-const Restaurants = require("../models/restaurant.model");
 
-router.get("/", async (req, res) => {
-  try {
-    const restaurants = await Restaurants.find();
+router.get("/", getRestaurants);
 
-    res.status(200).json({
-      success: true,
-      count: restaurants.length,
-      restaurants,
-    });
-  } catch (err) {
-    console.error("Failed to fetch restuarants", err);
-
-    res.status(500).json({
-      success: false,
-      msg: "Failed to fetch restuarants",
-    });
-  }
-});
-
-router.get("/:resId", async (req, res) => {
-  try {
-    const { resId } = req.params;
-
-    const restaurant = await Restaurants.findOne({ restaurantId:resId });
-
-    if (!restaurant) {
-      return res.status(404).json({
-        success: false,
-        msg: "Restaurant not  found",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      restaurant,
-    });
-  } catch (err) {
-    console.error("Failed to fetch restaurant", err);
-
-    res.status(500).json({
-      success: false,
-      message: "Failed to fetch restaurant",
-    });
-  }
-});
+router.get("/:resId", getRestaurantById);
 
 module.exports = router;
